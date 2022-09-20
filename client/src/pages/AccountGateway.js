d244c9128112b9d845cdf1564166ef6b1bda066e
OmniOmni <austin.olijar@gmail.com>
2022-09-20 17:40:49 -0500

    sign in or added

--- client/src/pages/AccountGateway.js
+++ client/src/pages/AccountGateway.js
@@ -4,7 +4,6 @@ import AccountSignIn from "../components/AccountSignIn";
 import AccountSignInGoogle from "../components/AccountSignInGoogle";
 import { auth } from "../firebase";
 
-
 const AccountGateway = () => {
   const [activeSignUp, setActiveSignUp] = useState(false);
 
@@ -21,7 +20,6 @@ const AccountGateway = () => {
     setActiveSignUp(false);
   };
 
-
   return (
     <section className="flex items-center justify-center flex-col">
       <div
@@ -35,13 +33,14 @@ const AccountGateway = () => {
           handleActiveSignIn={handleActiveSignIn}
           activeSignUp={activeSignUp}
         />
-
         <AccountSignUp
           handleActiveSignUp={handleActiveSignUp}
           activeSignUp={activeSignUp}
         />
       </div>
-      <AccountSignInGoogle auth={auth}/>
+      
+      <p className="bg-white relative after:absolute after:h-[1px] after:w-[40%] after:right-[5%] after:top-1/2 after:bg-slate-300 before:absolute before:h-[1px] before:w-[40%] before:left-[5%] before:top-1/2 before:bg-slate-300 text-black w-screen sm:w-96 text-center">or</p><div></div>
+      <AccountSignInGoogle auth={auth} />
     </section>
   );
 };

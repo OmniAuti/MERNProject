f3cfecc17102739964be69c692518cbb67c2cb39
OmniOmni <austin.olijar@gmail.com>
2022-10-04 15:26:40 -0500

    messages scroll to bottom

--- client/src/components/ChatMessages.js
+++ client/src/components/ChatMessages.js
@@ -1,6 +1,6 @@
 const ChatMessages = ({ msg, user }) => {
   return (
-    <div className={user.uid === msg.uidInitiated ? "bg-sky-200 w-fit px-2 rounded-md m-1 border border-black self-end max-w-1/2 text-right" : "bg-green-200 w-fit max-w-1/2 px-2 rounded-md m-1 border border-black text-left"}>
+    <div className={user.uid === msg.uidInitiated ? "bg-sky-200 w-fit px-2 rounded-md m-1 border border-black self-end max-w-1/2 text-right first-of-type:mt-auto" : "bg-green-200 w-fit max-w-1/2 px-2 rounded-md m-1 border self-start border-black text-left first-of-type:mt-auto"}>
       <p className="text-black">{msg.message}</p>
     </div>
   );
